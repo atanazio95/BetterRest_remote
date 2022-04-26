1584317d86e1a5feb7ba5b91a2f3c5a00ad73b47
Jeorge Atanazio <jeorge.atanazio@iteris.com.br>
2022-04-26 14:10:53 -0300

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Jeorge Antonio Atanazio Santos - JAT on 25/04/22.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
